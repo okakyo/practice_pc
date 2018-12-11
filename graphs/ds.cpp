ea1f4672b34c3afff3500340857066d5a8070256
Kyohei Oka <0622okakyo@gmail.com>
2018-12-11 20:12:08 +0900

    '練習中'

---
+++ graphs/ds.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+#include<map>
+#include<set>
+#include<vector>
+
+using namespace std;
+
+int main(){
+	int v,e,r;
+	vector<int> ans;
+	scanf("%d%d%d",&v,&e,&r);
+	for(int i=0;i<e,i++){
+		int s,t,d;		
+		scanf("%d%d%d",&s,&t,&d);	
+	}
+
+return 0;
+}
