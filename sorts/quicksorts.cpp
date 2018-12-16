ebaf4adf14c8d09dd2bc4e6a4cb02b757ca05381
Kyohei Oka <0622okakyo@gmail.com>
2018-12-16 21:35:26 +0900

    '練習中'

---
+++ sorts/quicksorts.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+void sorts(int *a[]){
+	
+
+}
+
+int main{
+	int D[]={1,0,3,5,8,10};
+	
+
+
+return 0;
+
+}
