d9bd2be2b2782838dce8686f02e76e43e4949cf0
Kyohei Oka <0622okakyo@gmail.com>
2018-12-19 18:23:07 +0900

    '練習中'

---
+++ puzzle/suudoku.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<string>
+#include<stdlib>
+
+using namespace std;
+
+string field[9][9];
+
+int main(){
+	for(int i=0;i<9;i++)	
+	for(int j=0;j<9;j++)	
+		scanf("%s",&field[i][j]);
+	
+	for(int i=0;i<9;i++)	
+	for(int j=0;j<9;j++)
+	if(field[i][j]!=".")
+
+
+return 0;
+}
