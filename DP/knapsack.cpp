dde0066b1ad4c8ea70f83c8af9173676b70dd892
Kyohei Oka <0622okakyo@gmail.com>
2019-01-09 11:42:24 +0900

    '練習中'

--- DP/knapsack.cpp
+++ DP/knapsack.cpp
@@ -1,24 +1,13 @@
-#include<cstdio>
+#include<iostream>
 #include<algorithm>
 
 using namespace std;
 
-const int MAX_N=100;
-
-int n,W;
-int w[MAX_N], v[MAX_N];
-
-int  rec(int i,int j){
-	int res;
-	if(i==n)res=0;
-	else if(j<w[i])res=rec(i+1,j);
-	else res=max(rec(i+1,j),rec(i+1,j-w[i])+v[i]);
-return res;
-}
-
 int main(){
-	scanf("%d%d",&n,&W);
-
-printf("%d\n",rec(0,W));
+	int MAX_N;
+	cin<<MAX_N<<endl;
+	for(int i=0;i<N;i++)
+		dp[i]=1;
+		
 return 0;
 }
